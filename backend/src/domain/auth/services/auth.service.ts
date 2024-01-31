f925e56848f6653ce53a3b8b7764fafb891f22e1
Alex Phloyd <alexphloyd@gmail.com>
2024-01-31 23:43:03 +0100

    update backend configs and add eslint

--- backend/src/domain/auth/services/auth.service.ts
+++ backend/src/domain/auth/services/auth.service.ts
@@ -1,48 +1,34 @@
-import { extractTokenFromHeader } from "@app/domain/auth/lib/extractTokenFromHeader";
-import { HashService } from "@app/domain/auth/services/hash.service";
-import { UserRepository } from "@app/domain/user/services/user.repository";
-import { LoginSchema } from "dto";
-import { Injectable, HttpException } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
-import { HttpStatusCode } from "axios";
-import { Request } from "express";
-import { z } from "nestjs-zod/z";
+import { extractTokenFromHeader } from '@app/domain/auth/lib/extractTokenFromHeader';
+import { HashService } from '@app/domain/auth/services/hash.service';
+import { UserRepository } from '@app/domain/user/services/user.repository';
+import { LoginSchema } from 'dto';
+import { Injectable, HttpException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { HttpStatusCode } from 'axios';
+import { Request } from 'express';
+import { z } from 'nestjs-zod/z';
 
 @Injectable()
 export class AuthService {
   constructor(
     private hashService: HashService,
     private jwtService: JwtService,
-    private userRepository: UserRepository
+    private userRepository: UserRepository,
   ) {}
 
   async login({ email, password }: z.infer<typeof LoginSchema>) {
     const user = await this.userRepository.findByEmail({ email });
 
-    if (!user)
-      throw new HttpException("Invalid credentials", HttpStatusCode.Conflict);
+    if (!user) throw new HttpException('Invalid credentials', HttpStatusCode.Conflict);
 
-    const isPasswordMatch = await this.hashService.compare(
-      password,
-      user.password
-    );
-    if (!isPasswordMatch)
-      throw new HttpException("Invalid credentials", HttpStatusCode.Conflict);
+    const isPasswordMatch = await this.hashService.compare(password, user.password);
+    if (!isPasswordMatch) throw new HttpException('Invalid credentials', HttpStatusCode.Conflict);
 
     if (!user.verified)
-      throw new HttpException(
-        "Please, verify your account",
-        HttpStatusCode.UpgradeRequired
-      );
+      throw new HttpException('Please, verify your account', HttpStatusCode.UpgradeRequired);
 
-    const access = this.jwtService.sign(
-      { sub: user.id, role: user.role },
-      { expiresIn: "4m" }
-    );
-    const refresh = this.jwtService.sign(
-      { sub: user.id, role: user.role },
-      { expiresIn: "1d" }
-    );
+    const access = this.jwtService.sign({ sub: user.id, role: user.role }, { expiresIn: '4m' });
+    const refresh = this.jwtService.sign({ sub: user.id, role: user.role }, { expiresIn: '1d' });
 
     return {
       tokens: {
@@ -54,15 +40,14 @@ export class AuthService {
   }
 
   async refresh(req: Request) {
-    const token = req?.cookies["refresh"];
+    const token = req?.cookies['refresh'];
     const verified = await this.jwtService.verifyAsync(token);
 
-    if (!verified)
-      throw new HttpException("Invalid token", HttpStatusCode.Locked);
+    if (!verified) throw new HttpException('Invalid token', HttpStatusCode.Locked);
 
     const { sub, role } = verified;
-    const access = this.jwtService.sign({ sub, role }, { expiresIn: "4m" });
-    const refresh = this.jwtService.sign({ sub, role }, { expiresIn: "1d" });
+    const access = this.jwtService.sign({ sub, role }, { expiresIn: '4m' });
+    const refresh = this.jwtService.sign({ sub, role }, { expiresIn: '1d' });
 
     return {
       access,
@@ -72,11 +57,10 @@ export class AuthService {
 
   async checkSession(req: Request) {
     const bearer = extractTokenFromHeader(req);
-    if (!bearer)
-      throw new HttpException("Invalid token", HttpStatusCode.Locked);
+    if (!bearer) throw new HttpException('Invalid token', HttpStatusCode.Locked);
 
     const { sub } = await this.jwtService.verifyAsync(bearer).catch(() => {
-      throw new HttpException("Unauthorized", HttpStatusCode.Unauthorized);
+      throw new HttpException('Unauthorized', HttpStatusCode.Unauthorized);
     });
 
     const sessionUser = await this.userRepository.findById({
@@ -93,8 +77,7 @@ export class AuthService {
       },
     });
 
-    if (!sessionUser)
-      throw new HttpException("Unauthorized", HttpStatusCode.Locked);
+    if (!sessionUser) throw new HttpException('Unauthorized', HttpStatusCode.Locked);
 
     return {
       user: sessionUser,
