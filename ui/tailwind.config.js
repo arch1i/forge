381742b23ca9efbcb5a13699e7ccb22a41112c65
Alex Phloyd <alexphloyd@gmail.com>
2024-02-01 19:20:02 +0100

    add tailwind

---
+++ ui/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{tsx,jsx,ts}'],
+  theme: {},
+
+  plugins: [],
+
+  important: '#root',
+  corePlugins: {
+    preflight: false,
+  },
+};
