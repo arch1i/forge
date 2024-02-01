454fad0c89a42f8fbc4ab9aa1f203b2c64e0112f
Alex Phloyd <alexphloyd@gmail.com>
2024-02-01 18:22:21 +0100

    add env config

---
+++ ui/src/shared/config/env.config.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+const envVariables = z.object({
+  VITE_AUTH_SERVICE_URL: z.string().url(),
+});
+
+envVariables.parse(import.meta.env);
+
+export const ENV_CONFIG = {
+  AUTH_SERVICE_URL: import.meta.env.VITE_AUTH_SERVICE_URL,
+} as const;
