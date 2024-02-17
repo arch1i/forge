4ab97ceef7f139a320f1b730c4051dfdfe506d06
alexphloyd <alexphloyd@gmail.com>
2024-02-17 16:13:10 +0100

    add menu-button transparent variant

--- ui/src/shared/ui/menu-button.tsx
+++ ui/src/shared/ui/menu-button.tsx
@@ -11,36 +11,44 @@ interface Props extends VariantProps<typeof styles> {
 }
 
 export const MenuButton = (props: Props) => {
-    const { content, className, color, size, disabled = false, onClick, htmlType = 'button' } = props;
+    const { content, className, type, size, disabled = false, onClick, htmlType = 'button' } = props;
 
     return (
         <button
             disabled={disabled}
             onClick={onClick}
             type={htmlType}
-            className={twMerge(styles({ color, size }), className)}
+            className={twMerge(styles({ type, size, disabled }), className)}
         >
             {content}
         </button>
     );
 };
 
-const styles = cva(
-    'text-center w-fit rounded-[4px] whitespace-nowrap flex items-center justify-center',
-    {
-        variants: {
-            color: {
-                light: [
-                    'text-[#1b1b1f] bg-[#ececf3] hover:bg-[#f1f0fe] active:outline active:outline-1 active:outline-[#4843b9] cursor-pointer',
-                ],
-            },
-            size: {
-                md: ['px-2 py-2'],
-            },
+const styles = cva('w-fit rounded-[4px] whitespace-nowrap tracking-wider font-sans', {
+    variants: {
+        type: {
+            light: [
+                'text-[#1b1b1f] bg-[#ececf3] cursor-pointer flex items-center justify-center text-center',
+                'hover:bg-[#f1f0fe] active:outline active:outline-1 active:outline-[#4843b9]',
+            ],
+
+            transparent: [
+                'text-sm text-[#1b1b1f] bg-transparent cursor-pointer text-left',
+                'hover:bg-[#f1f0fe] active:outline active:outline-1 active:outline-[#4843b9]',
+            ],
         },
-        defaultVariants: {
-            color: 'light',
-            size: 'md',
+        size: {
+            md: ['px-2 py-2'],
+            lg: ['px-[13px] py-[6px]'],
+        },
+        disabled: {
+            true: ['cursor-not-allowed hover:none active:none'],
         },
     },
-);
+
+    defaultVariants: {
+        type: 'light',
+        size: 'md',
+    },
+});
