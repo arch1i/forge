1cf69c4c0a7125530df011dd704e2b2984fb5840
alexphloyd <alexphloyd@gmail.com>
2024-02-19 22:09:34 +0100

    add app-start-listening type

--- ui/src/app/store/types.d.ts
+++ ui/src/app/store/types.d.ts
@@ -1,5 +1,9 @@
+import { type TypedStartListening } from '@reduxjs/toolkit';
+
 declare type Store = ReturnType<typeof import('./app-store').createStore>['store'];
 
 declare type AppState = ReturnType<Store['getState']>;
 
 declare type Dispatch = Store['dispatch'];
+
+declare type AppStartListening = TypedStartListening<AppState, Dispatch>;
