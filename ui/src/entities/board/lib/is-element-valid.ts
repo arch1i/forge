10a1dec19c1d97c3a2565dbbf349f8c4f4dc4abf
alexphloyd <alexphloyd@gmail.com>
2024-02-19 18:27:46 +0100

    add is-element-valid

---
+++ ui/src/entities/board/lib/is-element-valid.ts
@@ -0,0 +1,8 @@
+import { MIN_ELEMENT_SIZE } from '../model/constants';
+import { Element } from '../model/types/element';
+
+export function isElementValid(element: Element) {
+    if (element.size.width < MIN_ELEMENT_SIZE && element.size.height < MIN_ELEMENT_SIZE) return false;
+
+    return true;
+}
