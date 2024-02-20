8b7f4a6427038509b1713bdbba92a86e8c1065b8
alexphloyd <alexphloyd@gmail.com>
2024-02-20 11:10:56 +0100

    refactor: app listener

--- ui/src/app/store/middleware.ts
+++ ui/src/app/store/middleware.ts
@@ -1,3 +1,5 @@
-import { createListenerMiddleware } from '@reduxjs/toolkit';
+import { type TypedStartListening, createListenerMiddleware } from '@reduxjs/toolkit';
 
 export const listener = createListenerMiddleware();
+
+export const on = listener.startListening as TypedStartListening<AppState, Dispatch>;
