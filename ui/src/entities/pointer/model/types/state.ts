92dd170db8034bcd6c170fba13e524937f0dac55
alexphloyd <alexphloyd@gmail.com>
2024-02-25 23:59:07 +0100

    add modifying-board-view pointer status

--- ui/src/entities/pointer/model/types/state.ts
+++ ui/src/entities/pointer/model/types/state.ts
@@ -3,13 +3,16 @@ import { type ComputedPointerPosition } from './core';
 
 export type Pointer = {
     mode: 'default' | ElementType;
-    status: 'idle' | 'drafting-an-element';
+    status: 'idle' | 'drafting-an-element' | 'modifying-board-view';
     info: {
         'drafting-an-element'?: {
             mode: DraftingMode;
             elementKey: UniqueKey;
             initialPointerPosition?: ComputedPointerPosition;
             initialElementPosition?: ComputedPointerPosition;
+        };
+        'modifying-board-view'?: {
+            lastTouchPoints?: Array<ComputedPointerPosition>;
         };
     };
 
